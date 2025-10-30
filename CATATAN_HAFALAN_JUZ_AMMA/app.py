b369a7d791e55faf9f42619f1c0aaf834c92dafb
axsapari <axsapari@gmail.com>
2025-10-30 12:41:17 +0700

    Update app.py

--- CATATAN_HAFALAN_JUZ_AMMA/app.py
+++ CATATAN_HAFALAN_JUZ_AMMA/app.py
@@ -836,15 +836,6 @@ def sidebar_controls(df):
     kelas_list = ["Pilih Kelas"] + sorted(df["Kelas"].unique().tolist())
     selected_class = st.sidebar.selectbox("Kelas", kelas_list)
 
-    # --- identitas sekolah di sidebar ---
-    st.sidebar.markdown("---")
-    st.sidebar.markdown(
-        """
-        **Aplikasi Hafalan Juz Amma**  
-        _SMP Negeri 9 Banjar_  
-        Pengembang: **Agus Sugiharto Sapari, S.Pd.**  
-        © 2025
-        """
     )
 
     # ====================
@@ -1124,3 +1115,4 @@ if __name__ == "__main__":
         initialize_database(DB_FILE)
     main_app()
     show_footer()
+
